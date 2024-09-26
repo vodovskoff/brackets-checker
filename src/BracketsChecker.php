083a45b2283570176db29236fb11853807ba2015
Evgenii Vodovskov <vodovskov_ea@drom.ru>
2024-09-26 13:05:24 +1000

    Чуть ускорить

--- src/BracketsChecker.php
+++ src/BracketsChecker.php
@@ -6,28 +6,36 @@ namespace VodovskovEa\BracketsChecker;
 
 class BracketsChecker
 {
+    private array $closeBracketSet = [];
+    private array $openBracketSet = [];
+
     private const CLOSE_BRACKET_TO_OPEN_BRACKET = [
         ')' => '(',
         '}' => '{',
         ']' => '[',
     ];
 
+    public function __construct()
+    {
+        foreach (self::CLOSE_BRACKET_TO_OPEN_BRACKET as $closeBracket => $openBracket) {
+            $this->openBracketSet[$openBracket] = true;
+            $this->closeBracketSet[$closeBracket] = true;
+        }
+    }
+
     public function check(string $string): bool
     {
         $stack = [];
         $iMax = strlen($string);
+
         for ($i = 0; $i < $iMax; ++$i) {
             $char = $string[$i];
-            if (!in_array($char, array_merge(array_keys(self::CLOSE_BRACKET_TO_OPEN_BRACKET), array_values(self::CLOSE_BRACKET_TO_OPEN_BRACKET)), true)) {
-                continue;
-            }
-
-            if (in_array($char, array_values(self::CLOSE_BRACKET_TO_OPEN_BRACKET), true)) {
-                $stack[] = $char;
-            }
-
-            if (array_key_exists($char, self::CLOSE_BRACKET_TO_OPEN_BRACKET) && array_pop($stack) !== self::CLOSE_BRACKET_TO_OPEN_BRACKET[$char]) {
+            if (isset($this->closeBracketSet[$char]) && array_pop($stack) !== self::CLOSE_BRACKET_TO_OPEN_BRACKET[$char]) {
                 return false;
+            }
+
+            if (isset($this->openBracketSet[$char])) {
+                $stack[] = $char;
             }
         }
 
